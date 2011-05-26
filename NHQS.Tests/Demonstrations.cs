ecec0c9299c9948ab420d381182fac335d0129f5
brady gaster <brady.gaster@gmail.com>
2011-05-26 08:04:08 -0400

    added more examples to the unit tests to help with post #3 on the topic

--- NHQS.Tests/Demonstrations.cs
+++ NHQS.Tests/Demonstrations.cs
@@ -5,6 +5,7 @@ using System.Text;
 using NUnit.Framework;
 using People.DataAccess;
 using Orders.DataAccess;
+using People.Domain;
 
 namespace NHQS.Tests
 {
@@ -19,6 +20,57 @@ namespace NHQS.Tests
 
             SessionFactoryContainer.Current.Add(
                 new OrdersSessionFactoryCreator().Create());
+
+            SessionFactory
+                .For<Person>()
+                .OpenSession()
+                    .Save<Person>(new Person
+                    {
+                        FirstName = "test",
+                        LastName = "user"
+                    });
+        }
+
+        [Test]
+        public void data_was_saved()
+        {
+            Assert.IsTrue(
+                SessionFactory
+                    .For<Person>()
+                    .OpenSession()
+                        .Find<Person>(x => x.Id > 0)
+                            .Any()
+                        );
+        }
+
+        [Test]
+        public void data_known_to_exist_can_be_found()
+        {
+            Assert.IsTrue(
+                SessionFactory
+                    .For<Person>()
+                    .OpenSession()
+                        .Find<Person>(x => x.FirstName == "test")
+                            .Any()
+                        );
+        }
+
+        [Test]
+        public void multiple_functions_can_be_performed_fluently()
+        {
+            var firstName = "test";
+
+            SessionFactory
+                    .For<Person>()
+                    .OpenSession()
+                        .Find<Person>(x => x.FirstName == firstName,
+                            people =>
+                                Assert.That(people.Any()))
+                        .Delete<Person>(x =>
+                            x.FirstName == firstName)
+                        .Find<Person>(x =>
+                            x.FirstName == firstName, people =>
+                                Assert.False(people.Any()));
         }
     }
 }
