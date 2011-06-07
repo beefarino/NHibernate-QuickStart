2188591c340fce657d80a0defa51ec02a28fbb63
brady gaster <brady.gaster@gmail.com>
2011-06-07 15:57:46 -0400

    added in the domain console components

---
+++ Powershell/DriveParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+
+namespace NHQS.DriveProvider
+{
+    public class DriveParameters
+    {
+        [Parameter(Mandatory = false)]
+        public string DomainAssembly { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public string DataAccessAssembly { get; set; }
+    }
+}
